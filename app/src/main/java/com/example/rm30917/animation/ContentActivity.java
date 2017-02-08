a4457ba2235b18ace78b94e802c0ca2c08b6c8c5
Caue Bruno de Almeida <RM30917@fiap.com.br>
2017-02-08 21:23:16 -0200

    GIF Animation: frame by frame broooo

--- app/src/main/java/com/example/rm30917/animation/ContentActivity.java
+++ app/src/main/java/com/example/rm30917/animation/ContentActivity.java
@@ -1,13 +1,24 @@
 package com.example.rm30917.animation;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.widget.ImageView;
 
 public class ContentActivity extends AppCompatActivity {
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content);
+
+        imageView = (ImageView) findViewById(R.id.bananaImageView);
+        imageView.setBackgroundResource(R.drawable.banana);
+
+        AnimationDrawable frameAnimation = (AnimationDrawable) imageView.getBackground();
+        frameAnimation.start();
     }
 }
--- readme.md
+++ readme.md
@@ -3,4 +3,4 @@
 Whaaaaaaaaaaaat?
 
 - Check something about [Butterknife](http://jakewharton.github.io/butterknife/) (???) - talking about card appearing after each other
-
+- [Chaves](http://tutorialandroid.com.br/download/imagens-animacao-chaves/)
